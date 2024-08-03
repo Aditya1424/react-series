f500d0cb8a69a53eda782b2bc34e0256c7092f2d
ADITYA GUPTA <ag7023587@gmail.com>
2024-08-04 01:03:21 +0530

    creating function in different way

--- 01-basics/src/main.jsx
+++ 01-basics/src/main.jsx
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+function MyApp(){
+  return (
+    <div>
+      <h1>My First React App</h1>
+      <p>This is a simple React application!!</p>
+    </div>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ 
+    MyApp()
+ 
 )
