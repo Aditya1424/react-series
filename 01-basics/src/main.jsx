a68663cd205e0d15280f705424d1ad939e29c659
ADITYA GUPTA <ag7023587@gmail.com>
2024-08-04 01:16:27 +0530

    understand createElement method

--- 01-basics/src/main.jsx
+++ 01-basics/src/main.jsx
@@ -12,8 +12,16 @@ function MyApp(){
   )
 }
 
+// Using React.createElement
+// have 3 parameters in it ('tag', {attributes:'eg'}, 'Text')
+const reactElement = React.createElement(
+  'h1',
+  { className: 'title' },
+  'Hello, React!'
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
  
-    MyApp()
+  reactElement
  
 )
