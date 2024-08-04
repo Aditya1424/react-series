58822a01c16ac1c9ae4cf1e940d194a22e3c6c7d
ADITYA GUPTA <ag7023587@gmail.com>
2024-08-05 03:20:07 +0530

    add something

--- 02-counter/src/App.jsx
+++ 02-counter/src/App.jsx
@@ -8,13 +8,13 @@ function App() {
   // let counter = 5
 
   // using useState hook to manage the state of counter
-  // this hook return an array with 2 argument ,
+  // this hook return an array with 2 arguments ,
   // one is the state/value and another one is the function that helps to change the value/state
   let [counter, setCounter]= useState(10)
 
 
   const addValue = () => {
-    // by simply increasing the counter value we are not able to upadate the value in our UI
+    // by simply increasing the counter value, we are not able to upadate the value in our UI
     // instead, we will update the value using the useState hook
 
     if(counter>=20){
