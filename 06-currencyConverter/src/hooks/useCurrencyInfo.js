35087b884912f2c4e499ae0629d79f759c805317
ADITYA GUPTA <ag7023587@gmail.com>
2024-08-11 22:42:09 +0530

    completed the project

--- 06-currencyConverter/src/hooks/useCurrencyInfo.js
+++ 06-currencyConverter/src/hooks/useCurrencyInfo.js
@@ -6,9 +6,8 @@ function useCurrencyInfo(currency){
 
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-           .then(res => res.json())
-           .then(res => setInfo(res[currency]))
-           .catch(error => console.error('Error:', error));
+           .then((res) => res.json())
+           .then((res) => setInfo(res[currency]))
     }, [currency])
 
     return info;
