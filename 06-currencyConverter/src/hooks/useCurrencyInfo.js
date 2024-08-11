143b04366a1bc35e69eb3c4e6a89422d1aca320b
ADITYA GUPTA <ag7023587@gmail.com>
2024-08-11 18:28:54 +0530

    create our own custom hook

---
+++ 06-currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,17 @@
+import {useState, useEffect} from 'react';
+
+// custom hooks--> making your own hook
+function useCurrencyInfo(currency){
+    const [info, setInfo] = useState({})
+
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+           .then(res => res.json())
+           .then(res => setInfo(res[currency]))
+           .catch(error => console.error('Error:', error));
+    }, [currency])
+
+    return info;
+}
+
+export default useCurrencyInfo;
